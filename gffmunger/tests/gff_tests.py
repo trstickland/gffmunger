e34732142a26f8d93786e3a7ae42d5c5ec935472
Tim Stickland <T.R.Stickland@gmail.com>
2018-06-29 14:12:47 +0100

    finished gffutils tests

--- gffmunger/tests/gff_tests.py
+++ gffmunger/tests/gff_tests.py
@@ -5,45 +5,80 @@ import warnings
 
 test_modules_dir  = os.path.dirname(   os.path.realpath( __file__ ) )
 data_dir          = os.path.join(      test_modules_dir, 'data' )
-test_gff_file     = os.path.join(      data_dir,         'SAMPLE.gff3' )
+test_gff_file     = os.path.join(      data_dir,         'SAMPLE.gff3.gz' )
 test_gff_db_file  = os.path.join(      data_dir,         'gffutils_test.db' )
 
-sample_gff_gene_id   = 'PF3D7_0100100'
+sample_gff_gene_id      = 'PF3D7_0100100'
+sample_gff_featuretypes = ['gene', 'mRNA', 'CDS', 'polypeptide'] 
 
-expected_db_class    = gffutils.FeatureDB
-expected_gene_class  = gffutils.Feature
+expected_db_class          = gffutils.FeatureDB
+expected_feature_class     = gffutils.Feature
+expected_attributes_class  = gffutils.attributes.Attributes
 
 class GFF_Tests(unittest.TestCase):
    
    test_gff_db    = None
+   db_available   = False
    
-   def setUp(self):
+   @classmethod
+   def setUpClass(self):
       with warnings.catch_warnings():
          warnings.filterwarnings("ignore", "unclosed file <_io\.TextIOWrapper",  ResourceWarning,           "gffutils", 133 )
          warnings.filterwarnings("ignore", "generator '_FileIterator\.",         PendingDeprecationWarning, "gffutils", 186 )
          warnings.filterwarnings("ignore", "unclosed file <_io\.TextIOWrapper",  ResourceWarning,           "gffutils", 668 )
          self.test_gff_db = gffutils.create_db( test_gff_file,
                                                 dbfn                    = test_gff_db_file,
-                                                force                   = True,     # overwrite previous testing db
-                                                keep_order              = True,
-                                                merge_strategy          = 'merge',
-                                                sort_attribute_values   = True
+                                                force                   = True,     # overwrite previous testing db file
+                                                merge_strategy          = 'error',
+                                                keep_order              = False,    # True doesn't appear to maintain attribute order :-/  (and turning this off may be faster)
+                                                sort_attribute_values   = False
                                                 )
-         
-   def tearDown(self):
-      #self.test_gff_db.close()
-      del self.test_gff_db
-      
+         if(self.test_gff_db is not None and isinstance(self.test_gff_db, expected_db_class)):
+            self.db_available = True
+
+                        
    def test_000_db_created(self):
       """test that gffutils.create_db was able to create a database from the sample GFF file"""
-      self.assertIsNotNone(self.test_gff_db)
-      self.assertIsInstance(self.test_gff_db, expected_db_class)
-
-   #@unittest.skipUnless(test_gff_db, "no db available")
+      #self.assertIsNotNone(self.test_gff_db)
+      #self.assertIsInstance(self.test_gff_db, expected_db_class)
+      self.assertTrue(self.db_available)
+      
+   #@unittest.skipUnless(db_available, "no db available")
    def test_010_gffutils_find_gene(self):
       """test that gffutils can find a sample gene identifier from the GFF"""
-      gene = self.test_gff_db[sample_gff_gene_id]
-      self.assertIsNotNone(gene)
-      self.assertIsInstance(gene, expected_gene_class)
-      
-      
+      if (not self.db_available):
+         self.skipTest('no db available')
+      this_gene = self.test_gff_db[sample_gff_gene_id]
+      self.assertIsNotNone(this_gene)
+      self.assertIsInstance(this_gene, expected_feature_class)
+
+
+   def test_020_gffutils_find_features(self):
+      """test that gffutils can find features in GFF"""
+      if (not self.db_available):
+         self.skipTest('no db available')
+      for this_featuretype in sample_gff_featuretypes:
+         for this_feature in self.test_gff_db.children(sample_gff_gene_id, featuretype=this_featuretype):
+            self.assertIsNotNone(this_feature)
+            self.assertIsInstance(this_feature, expected_feature_class)
+            # print(this_feature)
+
+
+   def test_030_gffutils_find_attributes(self):
+      """test that gffutils can find attributes in GFF"""
+      if (not self.db_available):
+         self.skipTest('no db available')
+      for this_featuretype in sample_gff_featuretypes:
+         for this_feature in self.test_gff_db.children(sample_gff_gene_id, featuretype=this_featuretype):
+            #print(this_feature)
+            self.assertIsNotNone(this_feature)
+            self.assertIsInstance(this_feature, expected_feature_class)
+            these_attributes = this_feature.attributes
+            self.assertIsNotNone(these_attributes)
+            self.assertIsInstance(these_attributes, expected_attributes_class)
+            for this_item in sorted(these_attributes.items()):
+               self.assertIsNotNone(this_item)
+               #print('{this_item[0]}: {this_item[1]}'.format(this_item=this_item))
+            
+            
+       

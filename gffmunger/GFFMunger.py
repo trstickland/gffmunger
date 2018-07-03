36eed0e84eab28b499bcbe51459ceb7afbe8c6bb
Tim Stickland <T.R.Stickland@gmail.com>
2018-07-03 17:01:06 +0100

    Moved GFF3 validation from test code into GFFMunger class.  Added config file, config.yml by default, and added a command line option '--config' to set alternative file name

--- gffmunger/GFFMunger.py
+++ gffmunger/GFFMunger.py
@@ -1,14 +1,36 @@
+import argparse
 import logging
 import os
+import subprocess
 import sys
 import time
+import yaml
 
 class GFFMunger:
    def __init__(self,options):
-      self.logger = logging.getLogger(__name__)
-      self.verbose                    = options.verbose
-      self.output_file                = options.output_file
       
+      # allow class to be constructed without options being defined
+      # intended for example to permit tests, not recommended for normal usage, for which the 'gffmunger' script is provided
+      if None == options:
+         self.verbose      = False
+         self.output_file  = 'gffmunger_output.gff3'
+         self.config_file  = 'config.yml'
+      else:
+         self.verbose      = options.verbose
+         self.output_file  = options.output_file
+         self.config_file  = options.config
+      
+      self.logger = logging.getLogger(__name__)
+      
+      config_fh = open( os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', self.config_file),
+                        'r'
+                        );
+      self.config = yaml.load(config_fh)
+      self.gt_path                  = self.config['gt_path']
+      self.gff3_validator_tool      = self.config['gff3_validator_tool']
+      self.gff3_valiation_timeout   = self.config['gff3_valiation_timeout']
+      config_fh.close()
+
       if self.output_file and os.path.exists(self.output_file):
          self.logger.error("The output file already exists, please choose another filename: "+ self.output_file)
          sys.exit(1)
@@ -20,3 +42,17 @@ class GFFMunger:
          
    def run(self):
       print(__name__+".run() was called")
+
+   def validate_GFF3(self, gff_filename, silent=False): 
+      """Validates GFF3 file.
+      Pass path of GFF3 file; if valid, True is returned; if invalid, validator STDERR output is printed and False is returned
+      Validator errors are printed to STDOUT; these can be supressed by passing the optional flag 'silent'"""
+      cp = subprocess.run( [self.gt_path, self.gff3_validator_tool, gff_filename],
+                           timeout=self.gff3_valiation_timeout,
+                           stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+      if 0 == cp.returncode:
+         return True
+      if not silent:
+         print(cp.stderr)
+      return False
+   
--- gffmunger/tests/validation_tests.py
+++ gffmunger/tests/validation_tests.py
@@ -2,39 +2,33 @@ import unittest
 import os
 import subprocess
 
+from gffmunger.GFFMunger import GFFMunger
+
 test_modules_dir  = os.path.dirname(   os.path.realpath( __file__ ) )
 data_dir          = os.path.join(      test_modules_dir, 'data' )
 test_gff_file     = os.path.join(      data_dir,         'SAMPLE.gff3.gz' )
 bad_gff_file      = os.path.join(      data_dir,         'NOT_GFF.gff3' )
 
-gt_cmd               = '/usr/bin/gt'
-test_arg             = '-help' # something guaranteed to be OK with any working install
-gff3_validator_tool  = 'gff3validator'
+gt_test_arg = '-help' # something guaranteed to be OK with any working install of genometools
 
 class GFF3Validator_Tests(unittest.TestCase):
    
-   #@classmethod
-   #def setUpClass(self):
-      ## whatever...
-                        
+   @classmethod
+   def setUpClass(self):
+      self.gffmunger = GFFMunger(None)
+          
    def test_000_gt_installed(self):
       """check genometools is installed and exceuteable"""
-      cp = subprocess.run([gt_cmd, test_arg], timeout=3, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+      cp = subprocess.run([self.gffmunger.gt_path, gt_test_arg], timeout=3, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
       success = 0 == cp.returncode
       if not success:
          print(cp.stdout)
       self.assertTrue(success)
-      
       
    def test_010_gt_valid_passes(self):
       """check genometools passes a valid GFF3 file"""
-      cp = subprocess.run([gt_cmd, gff3_validator_tool, test_gff_file], timeout=6, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-      success = 0 == cp.returncode
-      if not success:
-         print(cp.stdout)
-      self.assertTrue(success)
+      self.assertTrue( self.gffmunger.validate_GFF3(test_gff_file) )
 
    def test_020_gt_invalid_fails(self):
       """check genometools fails an invalid GFF3 file"""
-      cp = subprocess.run([gt_cmd, gff3_validator_tool, bad_gff_file], timeout=6, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-      self.assertFalse(0 == cp.returncode)
+      self.assertFalse( self.gffmunger.validate_GFF3(bad_gff_file, silent=True) ) # silence validation errors, which are expected

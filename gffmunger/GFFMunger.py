db1f1f1132e86df0d547b613aabbaae89d3401cb
Tim Stickland <T.R.Stickland@gmail.com>
2018-07-26 10:22:09 +0100

    stupid bug fix -- had accidentally merged out of date GFFMunger class :-/

--- gffmunger/GFFMunger.py
+++ gffmunger/GFFMunger.py
@@ -77,3 +77,55 @@ class GFFMunger:
          if not is_fasta and not silent:
             print(fasta_filename+" is not a valid FASTA file")
       return(is_fasta)
+   
+   def extract_GFF3_components(self, gff_filename):
+      """Extracts separate components of a GFF3 file:  metadata, features and FASTA
+      Stores these as raw text buffers, as read from the file, unescaped
+      - Metadata are lines at the beginning of the GFF3 starting '##'
+      - Features start from the first line that does not begin '##', and include following lines that start '#'
+        (which are human-readable comments rather than metadata).  
+      - FASTA data are read from the first line beginning '>', to the end of the file.  Note that  '>' is
+        *specifically* prohibited as the first character of a 
+      An artefact of this is that if there is a line such as '## FASTA' that indicate the start of FASTA data,
+      this will occur at the end of the feature buffer and not at the start of the FASTA buffer.  This is
+      safe as it will be ignored as a comment when parsing features (the GFF3 spec. requires parsers ignore
+      comments), whereas it could /(?? check)/ be a problem in the FASTA.  Hope this doesn't confuse anyone."""
+      self.input_metadata  = None
+      self.input_features  = None
+      self.input_fasta     = None
+      linenum              = 0
+      with self.open_text_file(gff_filename) as f:
+         # appends 'new' to 'buf', without error if buf is None
+         def append(new, buf=''):
+            if new is None:
+               return(buf)
+            return(buf + new)
+         for line in f:
+            linenum += 1
+            is_metadata = line.startswith('##')
+            # line looks like metadata, and haven't read any feature lines yet
+            # => add to metadata
+            if is_metadata and self.input_features is None:
+               #print("METADATA: "+line)
+               self.input_metadata = append(self.input_metadata, line)
+            # line looks like metadata, but we *have* read feature feature line(s)
+            # => treat as a comment, and add to features buffer
+            elif is_metadata and self.input_features is not None:
+               #print("FEATURE: "+line)
+               self.input_features = append(self.input_features, line)
+            # not metadata, and haven't read and FASTA yet
+            # => add to features
+            elif not is_metadata and not line.startswith('>') and self.input_fasta is None:
+               #print("FEATURE: "+line)
+               self.input_features = append(self.input_features, line)
+            # first line of FASTA, or any subsequent line
+            # => add to fasta
+            elif line.startswith('>') or self.input_fasta is not None:
+               #print("FASTA: "+line)
+               self.input_fasta = append(self.input_fasta, line)
+            else:
+               raise ValueError("Unexpected content in line {linenum} in GFF3:\n{line}")
+      return(linenum)
+      
+      
+      

bd8473417157bb5c783a6e8d7435071b4781b3f5
Tim Stickland <T.R.Stickland@gmail.com>
2018-08-15 11:35:50 +0100

    Change genometools path to /software/pathogen... install Allow genometools path to be set to alt paths with GENOMETOOLS_PATH environment variable or --genometools CLI argument Run tests with /usr/bin/gt in GENOMETOOLS_PATH

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,1 @@
-python3 -m unittest discover --start-directory gffmunger/tests/ --pattern  '*_tests.py' "$@"
+env GENOMETOOLS_PATH='/usr/bin/gt' python3 -m unittest discover --start-directory gffmunger/tests/ --pattern  '*_tests.py' "$@"
